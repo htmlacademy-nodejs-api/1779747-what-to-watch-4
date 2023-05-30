f77d5665bb37e64c33d7c8da4ad95f02bccab3ef
poluectov <poluectov@list.ru>
2023-05-30 21:06:17 +0300

    добавил команду generate, ,обновил --import

---
+++ src/types/mock-data.type.ts
@@ -0,0 +1,21 @@
+export type MockData = {
+    filmNames: string[];
+    descriptions: string[];
+    // postDate: Date;
+    genres: string[];
+    releaseDates: number[];
+    ratings: number[];
+    prevVideos: string[];
+    videos: string[];
+    actors: string[];
+    directors: string[];
+    durations: number[];
+    // comments: number[];
+    userNames: string[];
+    emails: string[];
+    avatarPaths?: string[];
+    passwords: string[];
+    posters: string[];
+    backgroundImages: string[];
+    backgroundColors: string[];
+  };
